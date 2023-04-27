21dde735bfc358d28dfb69f8737dfda430c4da1d
Alisson Araujo <alisson_araujo76@outlook.com>
2023-04-26 21:38:54 -0300

    Improving draw

--- main.c
+++ main.c
@@ -5,17 +5,21 @@ int main(int argc, char* argv[])
     int line=0;
     int column=0;
 
+    // Drawing board
+    printf(" _______\n");
+    printf("|       |\n");
+    printf("|  | |  |\n");
+    printf("|  | |  |\n");
+    printf("|  | |  |\n");
+    printf("|_______|\n");
+    printf("\n\n");
+
     printf("--> Player 1:\n");
 
     printf("Line: ");
     scanf("%d", &line);
     printf("Column: ");
     scanf("%d", &column);
-
-    // Drawing board
-    printf("\n\n | | \n");
-    printf(" | | \n");
-    printf(" | | \n");
 
 
     return 0;
